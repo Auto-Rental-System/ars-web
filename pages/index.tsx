509cbb639499ee60b629cb8d19d41c1d749e9060
Uryn Dmytro <dmytro@digia.co>
2022-12-21 05:38:10 +0200

    prettier run

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>NextJS Web Boilerplate</title>
-        <meta name="description" content="NextJS Web Boilerplate" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        NextJS Boilerplate
-      </main>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>NextJS Web Boilerplate</title>
+				<meta name='description' content='NextJS Web Boilerplate' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main>NextJS Boilerplate</main>
+		</>
+	);
 }
