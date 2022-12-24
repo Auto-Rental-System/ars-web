98b2baf124a3fbf6ca1c21e3777c5fc5899c5aea
Uryn Dmytro <dmytro@digia.co>
2022-12-24 22:25:22 +0200

    generate clients script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,15 @@
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import { ThemeProvider as MuiThemeProvider, StyledEngineProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider as MuiThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 
 import MuiStylesProvider from '@mui/styles/StylesProvider';
 
-import { GlobalStyle } from 'themes/globalStyles';
+import theme, { GlobalStyle } from 'themes';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-	const theme = createTheme({});
-
 	return (
 		<QueryClientProvider client={queryClient} contextSharing>
 			<MuiStylesProvider injectFirst>
